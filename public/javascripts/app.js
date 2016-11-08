fe778784ce76a36e2e8186ba1ab699c65fde713e
Jennifer Ball <jenniferpelota@gmail.com>
2016-11-08 16:11:00 -0700

    init change
    Add restaurants, change comments to restaurants, upvotes to votes, add rating

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,45 +1,55 @@
-angular.module('comment', [])
+angular.module('restaurant', [])
 .controller('MainCtrl', [
   '$scope','$http',
   function($scope,$http){
     $scope.test = 'Hello world!';
-    $scope.comments = [];
+    $scope.restaurants = [
+		{title:'Cubby\'s',rating:306*4,votes:306},
+		{title:'Hruska\'s Kolaches',rating:210*4.5,votes:210},
+		{title:'Cupbop Korean BBQ',rating:362*4,votes:362},
+		{title:'Cafe On Fire',rating:79*4.5,votes:79},
+		{title:'Molly\'s',rating:58*4.5,votes:58},
+		{title:'Taste',rating:53*4.5,votes:53},
+		{title:'Communal',rating:281*4.5,votes:281},
+		{title:'Black Sheep Cafe',rating:398*4,votes:398}
+	];
 
-    $scope.addComment = function() {
+    $scope.addRestaurant = function() {
        if($scope.formContent === '') { return; }
-      console.log("In addComment with "+$scope.formContent);
+      console.log("In addRestaurant with "+$scope.formContent);
       $scope.create({
         title: $scope.formContent,
-        upvotes: 0,
+		rating: 0,
+        votes: 0,
       });
       $scope.formContent = '';
     };
 
-    $scope.incrementUpvotes = function(comment) {
-      $scope.upvote(comment);
+    $scope.incrementVotes = function(restaurant) {
+      $scope.vote(restaurant);
     };
     
     $scope.getAll = function() {
-      return $http.get('/comments').success(function(data){
+      return $http.get('/restaurants').success(function(data){
 //        console.log("pass getAll");
-        angular.copy(data, $scope.comments);
+        angular.copy(data, $scope.restaurants);
       });
     };
     $scope.getAll();
 
-    $scope.create = function(comment) {
+    $scope.create = function(resturant) {
       //console.log("pass");
-      return $http.post('/comments', comment).success(function(data){
+      return $http.post('/comments', restaurant).success(function(data){
         //console.log(data);
-        $scope.comments.push(data);
+        $scope.restaurants.push(data);
       });
     };
 
-    $scope.upvote = function(comment) {
-      return $http.put('/comments/' + comment._id + '/upvote')
+    $scope.vote = function(restaurant) {
+      return $http.put('/restaurants/' + comment._id + '/upvote')
         .success(function(data){
           console.log("upvote worked");
-          comment.upvotes += 1;
+          restaurants.votes += 1;
         });
     };
 
